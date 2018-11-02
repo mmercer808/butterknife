29f546925613a3d70135bef3742b68c679a89e69
mmercer808 <mmerc808@gmail.com>
2018-11-01 20:49:50 -0700

    butterknife and snackbar

--- app/src/main/java/edu/galileo/mvp/LoginActivity.java
+++ app/src/main/java/edu/galileo/mvp/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.galileo.mvp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
@@ -96,6 +97,17 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     @Override
     public void successAction() {
         Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.show_snackbar)
+    public void showSnackBar() {
+        Snackbar.make(mLoginFormView, "THIS IS SIMPLE SNACKBAR", Snackbar.LENGTH_LONG)
+                .setAction("CLOSE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view){
+                        // ...
+                    }
+                }).show();
     }
 }
 
